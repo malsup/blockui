2c7061ed8fed4cd1c58a44c69fbc5c428b4c3d80
malsup <malsup@gmail.com>
2012-07-29 18:45:32 -0400

    merge

--- jquery.blockUI.js
+++ jquery.blockUI.js
@@ -253,20 +253,19 @@
 			var lyr3, s;
 			if (opts.theme && full) {
 				s = '<div class="blockUI ' + opts.blockMsgClass + ' blockPage ui-dialog ui-widget ui-corner-all" style="z-index:'+(z+10)+';display:none;position:fixed">';
-        if ( opts.title ) {
-				  s += '<div class="ui-widget-header ui-dialog-titlebar ui-corner-all blockTitle">'+(opts.title || '&nbsp;')+'</div>';
-        }
-        
-        s += '<div class="ui-widget-content ui-dialog-content"></div>' +
-					'</div>';
+	        	if ( opts.title ) {
+					s += '<div class="ui-widget-header ui-dialog-titlebar ui-corner-all blockTitle">'+(opts.title || '&nbsp;')+'</div>';
+	        	}
+		        s += '<div class="ui-widget-content ui-dialog-content"></div>';
+				s += '</div>';
 			}
 			else if (opts.theme) {
 				s = '<div class="blockUI ' + opts.blockMsgClass + ' blockElement ui-dialog ui-widget ui-corner-all" style="z-index:'+(z+10)+';display:none;position:absolute">';
-        if ( opts.title ) {
-          s += '<div class="ui-widget-header ui-dialog-titlebar ui-corner-all blockTitle">'+(opts.title || '&nbsp;')+'</div>'
-        }  
-        s += '<div class="ui-widget-content ui-dialog-content"></div>' +
-					'</div>';
+        		if ( opts.title ) {
+          			s += '<div class="ui-widget-header ui-dialog-titlebar ui-corner-all blockTitle">'+(opts.title || '&nbsp;')+'</div>'
+        		}  
+        		s += '<div class="ui-widget-content ui-dialog-content"></div>';
+				s += '</div>';
 			}
 			else if (full) {
 				s = '<div class="blockUI ' + opts.blockMsgClass + ' blockPage" style="z-index:'+(z+10)+';display:none;position:fixed"></div>';
